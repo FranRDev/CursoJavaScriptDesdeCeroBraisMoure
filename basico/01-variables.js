c474628d8bfbc365e22b4f781842fa1f78697710
FranRDev <franciscorodriguezgar@gmail.com>
2024-08-02 14:12:46 +0200

    Clase 1. Introducción, instalación, hola mundo y variables

---
+++ basico/01-variables.js
@@ -0,0 +1,24 @@
+// var
+
+var holaMundo = "¡Hola, JavaScript!"
+console.log(holaMundo)
+
+holaMundo = "¡Hola de nuevo, JavaScript!"
+console.log(holaMundo)
+
+// let
+
+let holaMundo2 = "¡Hola, JavaScript 2!"
+console.log(holaMundo2)
+
+holaMundo2 = "¡Hola de nuevo, JavaScript 2!"
+console.log(holaMundo2)
+
+// const
+
+const holaMundo3 = "¡Hola, JavaScript 3!"
+console.log(holaMundo3)
+
+// Error
+// holaMundo3 = "¡Hola de nuevo, JavaScript 3!"
+// console.log(holaMundo3)
