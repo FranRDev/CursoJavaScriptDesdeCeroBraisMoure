23b52bb373478f3dcd8defd02e72326c62a079bf
FranRDev <franciscorodriguezgar@gmail.com>
2024-08-07 14:12:45 +0200

    Clase 2. Ejercicios para principiantes

---
+++ basico/03-ejercicios-para-principiantes.js
@@ -0,0 +1,83 @@
+// 1. Escribe un comentario en una línea
+
+// Esto es un comentario de una línea.
+
+// 2. Escribe un comentario en varias líneas
+
+/*
+Esto es
+un comentario
+de varias líneas
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let cadena = "Soy una cadena"
+let numero = 1
+let soyBooleano = true
+let noDefinido = undefined
+let nulo = null
+let simbolo = Symbol("simbolo")
+let enteroGrande = 48946484846346764154658665584n
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(cadena)
+console.log(numero)
+console.log(soyBooleano)
+console.log(noDefinido)
+console.log(nulo)
+console.log(simbolo)
+console.log(enteroGrande)
+
+// 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof cadena)
+console.log(typeof numero)
+console.log(typeof soyBooleano)
+console.log(typeof noDefinido)
+console.log(typeof nulo)
+console.log(typeof simbolo)
+console.log(typeof enteroGrande)
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+cadena = "Hola mundo"
+numero = 2
+soyBooleano = false
+noDefinido = undefined
+nulo = null
+simbolo = Symbol("simbolo2")
+enteroGrande = 684635674456844535487543435874n
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+cadena = 1
+numero = "texto"
+soyBooleano = 654684634684348646346846843n
+noDefinido = true
+nulo = Symbol("nulo")
+simbolo = null
+enteroGrande = undefined
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+const cadena2 = "Hola espacio"
+const numero2 = 35
+const soyBooleano2 = true
+const noDefinido2 = undefined
+const nulo2 = null
+const simbolo2 = Symbol("simboloConstante")
+const enteroGrande2 = 614361748847196844656142321n
+
+// 9. A continuación, modifica los valores de las constantes
+
+// cadena2 = "Cadena modificada"
+// numero2 = 84
+// soyBooleano2 = false
+// noDefinido2 = undefined
+// nulo2 = null
+// simbolo2 = Symbol("simboloConstante2")
+// enteroGrande2 = 123456789012345678901234567890n
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarser
